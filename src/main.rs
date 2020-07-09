8ee7bcf75f5b105a422ec844616684549c7eae62
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-07-09 09:11:08 +0700

    menerapkan dotenv

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
+use std::env;
+use dotenv::dotenv;
 use hyper::{Client, Uri};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let url = "http://jsonplaceholder.typicode.com/users".parse::<Uri>()?;
+    dotenv().ok();
+
+    let data_url = env::var("FETCH_URL").expect("variabel FETCH_URL belum didefinisikan");
+    let url = data_url.parse::<Uri>()?;
     let client = Client::new();
     let resp = client.get(url).await?;
 
