3b4a538d5eca822eef7226fddaf2bcf09c8e4696
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-07-09 09:45:00 +0700

    fetch data body stream dan print konsol

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
 use dotenv::dotenv;
-use hyper::{Client, Uri};
+use hyper::{Client, Uri, body::HttpBody as _};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -9,9 +9,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let data_url = env::var("FETCH_URL").expect("variabel FETCH_URL belum didefinisikan");
     let url = data_url.parse::<Uri>()?;
     let client = Client::new();
-    let resp = client.get(url).await?;
+    let mut resp = client.get(url).await?;
 
-    println!("Response: {:?}", resp.body());
+    println!("Response: {:?}", resp.status());
+
+    // melakukan iterasi untuk StreamBody secara asynchronous
+    while let Some(chunk) = resp.body_mut().data().await {
+        let bin = chunk?;
+        let data = std::str::from_utf8(&bin)?;
+        println!("{}", data);
+    }
 
     Ok(())
 }
