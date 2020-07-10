e508a2db78af181d1c77ef5a388e69753b7ea2cc
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-07-10 20:12:53 +0700

    menyesuaikan hasil output json

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,6 @@ use hyper::{
     body::HttpBody as _
 };
 
-// const PARALLEL_REQUESTS: usize = 2;
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
@@ -23,26 +21,25 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = Client::new();
 
     let stream = stream::iter(1..=3);
-    stream.map(|x| {
+    let stream = stream.map(|x| {
         let uri = format!("{}{}", url, x);
 
         let resp = client.get(uri.parse().unwrap());
 
         resp
     }).then(|resp| async {
-        let hasil = resp.await.unwrap();
+        let mut hasil = resp.await.unwrap();
 
-        hasil
-    }).for_each(|mut body| async move {
-        while let Some(chunk) = body.body_mut().data().await {
-            let chunk = chunk.unwrap();
-            let obj_str = std::str::from_utf8(&chunk).unwrap();
-            let obj: Value = serde_json::from_str(&obj_str).unwrap();
+        let body_bin = hasil.body_mut().data().await.unwrap().unwrap();
+        let body_str = std::str::from_utf8(&body_bin).unwrap();
+        let body: Value = serde_json::from_str(&body_str).unwrap();
 
-            serde_json::to_writer_pretty(File::create("uji.json").unwrap(), &obj)
-                .expect("Tidak dapat membuat file json")
-        }
-    }).await;
+        body
+    });
+
+    let obj = stream.collect::<Vec<Value>>().await;
+
+    serde_json::to_writer_pretty(File::create("uji.json").unwrap(), &obj).unwrap();
 
     Ok(())
 }
