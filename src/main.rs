638acf55924443762ac6085f2f047c5319557e01
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-02-18 12:08:03 +0700

    minimalisasi main process

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,12 @@
-use std::env;
 mod models;
 mod handlers;
 mod helpers;
 
 use crate::models::error::AppErrors;
-use crate::handlers::fetching::konten;
+use crate::handlers::starting;
 
 
 #[tokio::main]
 async fn main() -> Result<(), AppErrors> {
-    let token_berita = env::var("NEWS_API")?;
-    // let bbta3_url = env::var("FETCH_BBTA3_URL")?;
-    // let bppt_url = env::var("FETCH_BPPT_URL")?;
-    let video_url = env::var("FETCH_VIDEO_URL")?;
-
-    let data = konten(
-        video_url.as_str(),
-        token_berita.as_str()
-    ).await?;
-
-    println!("{:?}", data);
-
-    Ok(())
+    starting::app().await
 }
